929d3aab6ade14c2947bb1c955badc130127ea1f
MaiGarzon <142328252+MaiGarzon@users.noreply.github.com>
2023-08-29 10:02:26 -0500

    Add files via upload

--- PrimerosProgramas/funciones/factorial.py
+++ PrimerosProgramas/funciones/factorial.py
@@ -1,1 +1,13 @@
+def factorial(a):
+    resultado=1
+    if a>1:
+      #resultado=a*factorial(a-1)
+      for i in range(1, a+1):
+         resultado=resultado*i
+    print('el resultado es: ', resultado)
+    return resultado
 
+a=int(input('Ingrese el numero: '))
+print('El numero es: ', a)
+valor=factorial(a)
+print(valor)

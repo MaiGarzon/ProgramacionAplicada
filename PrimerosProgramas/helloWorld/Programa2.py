26fd0c65ccb30cfc9ab8c9d05d5e3f297e3e8c19
MaiGarzon <142328252+MaiGarzon@users.noreply.github.com>
2023-09-21 15:15:24 -0500

    Update Programa2.py

--- PrimerosProgramas/helloWorld/Programa2.py
+++ PrimerosProgramas/helloWorld/Programa2.py
@@ -1,10 +1,8 @@
-# Define the release and runtime integer variables below:
-# Defina las variables enteras de liberación y tiempo de ejecución a continuación:
+# Definir las variables enteras de liberación y tiempo de ejecución a continuación:
 release_year = 1977
 runtime = 1980
 
-#Define the rating_out_of_10 float variable below:
-# Defina la variable flotante rating_out_of_10 a continuación:
+# Definir la variable flotante rating_out_of_10 a continuación:
 rating_out_of_10 = 6.8
 print(type(runtime))
 print(type(rating_out_of_10))

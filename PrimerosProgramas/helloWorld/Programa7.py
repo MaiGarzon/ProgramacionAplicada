c4af9b33e6cb706dcba0686b3ae8387791b32d21
MaiGarzon <142328252+MaiGarzon@users.noreply.github.com>
2023-09-21 15:30:43 -0500

    Update Programa7.py

--- PrimerosProgramas/helloWorld/Programa7.py
+++ PrimerosProgramas/helloWorld/Programa7.py
@@ -1,12 +1,10 @@
-# Prints 4 because 29 / 5 is 5 with a remainder of 4
+
 # Imprime 4 porque 29 / 5 es 5 con un resto de 4
 print(29%5)
 
-# Prints 2 because 32 / 3 is 10 with a remainder of 2
+# Imprime 2 porque 32/3 es 10 con un resto de 2
 print(32%3)
 
-# Modulo by 2 returns 0 for even numbers and 1 for odd numbers
-# Prints 0
 # Módulo por 2 devuelve 0 para números pares y 1 para números impares
-# Copias 0
+# imprime 0
 print(44%2)

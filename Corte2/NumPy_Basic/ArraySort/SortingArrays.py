0a2e260ffd6f6ece5c274be8e21dab828bceb1a0
MaiGarzon <142328252+MaiGarzon@users.noreply.github.com>
2023-11-19 22:56:11 -0500

    Create SortingArrays.py

---
+++ Corte2/NumPy_Basic/ArraySort/SortingArrays.py
@@ -0,0 +1,17 @@
+# Ejemplo 1
+
+import numpy as np
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
+
+# Ejemplo 2
+
+import numpy as np
+arr = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr))
+
+# Ejemplo 3
+
+import numpy as np
+arr = np.array([True, False, True])
+print(np.sort(arr))
